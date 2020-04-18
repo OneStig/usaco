f192b7f40c73dc5707b9ddb4a1bbf08f87bc4519
Steven He <therealonestig@gmail.com>
2020-04-18 16:32:50 -0700

    Fixed beads

--- beads/beads.cpp
+++ beads/beads.cpp
@@ -6,97 +6,66 @@
 
 using namespace std;
 
-int lb(string beads, int pos) {
-  int left = pos;
+int n;
+int cutOff = 0;
 
-  if (beads[pos] == 'W') {
-    return lb(beads, pos + 1);
+int evalLeft(string beads) {
+  char set = 'w';
+  int count = 0;
+
+  for (int i = 0; i < n; i++) {
+    if (set == 'w') {
+      set = beads[i];
+      count++;
+    } else if (beads[i] == set || beads[i] == 'w') {
+      count++;
+    } else {
+      break;
+      cutOff = i;
+    }
   }
-  else {
-    return pos;
-  }
+
+  return count;
 }
 
-int rb(string beads, int pos) {
-  int right = pos;
+int evalRight(string beads) {
+  char set = 'w';
+  int count = 0;
 
-  if (beads[right] == 'W') {
-    return rb(beads, pos - 1);
+  for (int i = n-1; i >= cutOff; i--) {
+    if (set == 'w') {
+      set = beads[i];
+      count++;
+    } else if (beads[i] == set || beads[i] == 'w') {
+      count++;
+    } else {
+      break;
+    }
   }
-  else {
-    return pos;
-  }
+
+  return count;
 }
 
 int main() {
   ofstream fout ("beads.out");
   ifstream fin ("beads.in");
 
-  int n;
   string neck;
   fin >> n;
   fin >> neck;
 
-  if (n == 0) {
-    fout << 0;
-  }
-  else {
-    // bool linked = (neck[lb(neck, 0)] == neck[rb(neck, n - 1)]);
+  int lg = 0;
 
-    list <int> groups;
-
-    string bneck = neck;
-    reverse(bneck.begin(), bneck.end());
-    neck += bneck;
-
-    groups.push_back(1);
-
-    char last = 'W';
-    int currentCount = 0;
-
-    for (int i = 0; i < n; i++) {
-      if (last == 'W') {
-        currentCount++;
-        last = neck[i];
-      } else if (neck[i] == last) {
-        currentCount++;
-      } else {
-        groups.push_back(currentCount);
-        currentCount = 1;
-        last = neck[i];
-      }
-    }
-
-
-    //if (linked) {
-    //  int f = groups.front() + currentCount;
-    //  groups.pop_front();
-    //  groups.push_front(f);
-    //}
-    //else {
-    //  groups.push_back(currentCount);
-    //}
-
-    groups.push_back(currentCount);
-
-    int largest = 0;
-
-    if (groups.size() >= 2) {
-      largest = groups.front() + groups.back();
-
-      for (int i = 0; i < groups.size() - 1; i++) {
-        int c = groups.front();
-        groups.pop_front();
-        c += groups.front();
-        if (c > largest) {
-          largest = c;
-        }
-      }
-
-      fout << largest;
-    } else  {
-      fout << groups.front();
+  for (int i = 0; i < n; i++) {
+    string splyce = neck.substr(i, n - i) + neck.substr(0, i);
+    cutOff = 0;
+    int t = evalLeft(splyce) + evalRight(splyce);
+    if (t > lg) {
+      lg = t;
     }
   }
+
+  fout << lg;
+
   return 0;
 }
