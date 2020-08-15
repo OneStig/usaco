f123b99e7dd842316093432731a3c39e6e919a79
Steven He <therealonestig@gmail.com>
2020-08-14 21:47:56 -0700

    Fracdec

--- fracdec/fracdec.cpp
+++ fracdec/fracdec.cpp
@@ -4,8 +4,8 @@ TASK: fracdec
 LANG: C++
 */
 
-#include <string>
 #include <fstream>
+#include <string>
 #include <map>
 #include <vector>
 #include <string>
@@ -16,74 +16,106 @@ using namespace std;
 ofstream fout("fracdec.out");
 ifstream fin("fracdec.in");
 
-int c = 0;
+int ccount = 0;
+
+void checkline()
+{
+    ccount++;
+    if (ccount % 76 == 0)
+    {
+        fout << endl;
+    }
+}
 
 int main()
 {
-    fin.open("fracdec.in");
-    fout.open("fracdec.out");
 
     int n, d;
     fin >> n >> d;
 
-    //print(n / d);
-    //print(".");
+    for (char ch : to_string(n / d))
+    {
+        fout << ch;
+        checkline();
+    }
+
+    fout << ".";
+    checkline();
 
     if (n % d == 0)
     {
-        //print("0");
-        if (c % 76 != 0)
+        fout << "0" << endl;
+        return 0;
+    }
+    else
+    {
+        map<int, int> m;
+        vector<int> nums;
+
+        int orig, value, remain = n % d;
+        bool moredig = true;
+
+        while (moredig)
+        {
+            orig = remain * 10;
+            value = orig / d;
+            remain = orig % d;
+
+            if (remain == 0)
+            {
+                for (int i = 0; i < nums.size(); i++)
+                {
+                    for (char ch : to_string(nums[i]))
+                    {
+                        fout << ch;
+                        checkline();
+                    }
+                }
+
+                for (char ch : to_string(value))
+                {
+                    fout << ch;
+                    checkline();
+                }
+
+                moredig = false;
+                break;
+            }
+
+            if (m.find(orig) == m.end())
+            {
+                m[orig] = nums.size();
+                nums.push_back(value);
+            }
+            else
+            {
+                int pos = m.find(orig)->second;
+                for (int i = 0; i < pos; i++)
+                {
+                    for (char ch : to_string(nums[i]))
+                    {
+                        fout << ch;
+                        checkline();
+                    }
+                }
+                fout << "(";
+                checkline();
+                for (int i = pos; i < nums.size(); i++)
+                {
+                    for (char ch : to_string(nums[i]))
+                    {
+                        fout << ch;
+                        checkline();
+                    }
+                }
+                fout << ")";
+                checkline();
+                break;
+            }
+        }
+        if (ccount % 76 != 0)
         {
             fout << endl;
         }
-        return 0;
     }
-
-    map<int, int> m;
-    vector<int> seq;
-
-    int org, val, mod = n % d;
-
-    while (true)
-    {
-        org = mod * 10;
-        val = org / d;
-        mod = org % d;
-
-        if (mod == 0)
-        {
-            for (int i = 0; i < seq.size(); i++)
-            {
-                //print(seq[i]);
-            }
-            //print(val);
-            break;
-        }
-
-        if (m.find(org) == m.end())
-        {
-            m[org] = seq.size();
-        }
-        else
-        {
-            int pos = m.find(org)->second;
-            for (int i = 0; i < pos; i++)
-            {
-                //print(seq[i]);
-            }
-            //print("(");
-            for (int i = pos; i < seq.size(); ++i)
-            {
-                //print(seq[i]);
-            }
-            //print(")");
-            break;
-        }
-    }
-    if (c % 76 != 0)
-    {
-        fout << endl;
-    }
-
-    return 0;
 }
