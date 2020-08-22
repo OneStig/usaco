ef29d3fa13baf829c8130db2b85f0607293f1104
Steven He <therealonestig@gmail.com>
2020-08-21 21:53:45 -0700

    Stamps unfinished

---
+++ stamps/stamps.cpp
@@ -0,0 +1,65 @@
+/*
+ID: stevenh6
+TASK: agrinet
+LANG: C++
+*/
+
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+ofstream fout("stamps.out");
+ifstream fin("stamps.in");
+
+int main()
+{
+    int stamps;
+    int lst[200];
+    int number;
+    int big = -1;
+
+    fin >> number >> stamps;
+    
+    for (int i = 0; i < stamps; ++i)
+    {
+        fin >> lst[i];
+        if (big < lst[i])
+        {
+            big = lst[i];
+        }
+    }
+    fin.close();
+
+    for (int i = 0; i <= big * number + 3; ++i)
+    {
+    }
+
+    cache[0] = 0;
+    for (int i = 0; i < stamps; ++i)
+    {
+        for (int j = 0; j <= big * number; ++j)
+        {
+            if (cache[j] < number)
+            {
+                if (cache[j + lst[i]] > 1 + cache[j])
+                {
+                    cache[j + lst[i]] = 1 + cache[j];
+                }
+            }
+        }
+    }
+
+    int str = 0;
+    while (cache[str + 1] <= number)
+    {
+        str++;
+    }
+
+    fout << str << endl;
+
+    return (0);
+}
