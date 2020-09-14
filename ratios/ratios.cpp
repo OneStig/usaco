9d2ccf3cb848d4fc42ef36092894739f264e2b9e
Steven He <therealonestig@gmail.com>
2020-09-13 21:59:17 -0700

    Ratios

--- ratios/ratios.cpp
+++ ratios/ratios.cpp
@@ -15,78 +15,75 @@ using namespace std;
 ofstream fout("ratios.out");
 ifstream fin("ratios.in");
 
-int read[3];
-int matr[3][3];
-int mloc[4];
-int sums[3];
-int minm;
+int goal[3];
+int ratios[3][3];
+int solm[4];
+int matr[3];
 
 int main()
 {
-    int s;
-    int rtot;
-    minm = 301;
+    int i, j, k, t, s;
+    int tot;
 
-    fin >> read[0] >> read[1] >> read[2];
-    rtot = read[0] + read[1] + read[2];
-    
-    for (int i = 0; i < 3; i++) {
-        fin >> matr[i][0] >> matr[i][1] >> matr[i][2];
-    }  
+    fin >> goal[0] >> goal[1] >> goal[2];
 
-    
+    for (i = 0; i < 3; i++)
+    {
+        fin >> ratios[i][0] >> ratios[i][1] >> ratios[i][2];
+    }
 
-    if (rtot == 0)
+    tot = goal[0] + goal[1] + goal[2];
+    int sol = 301;
+
+    if (tot == 0)
     {
         fout << "0 0 0 0" << endl;
         return 0;
     }
 
-    for (int i = 0; i < 100; i++)
+    for (i = 0; i < 100; i++)
     {
-        for (int j = 0; j < 100; j++)
+        for (j = 0; j < 100; j++)
         {
+            matr[0] = i * ratios[0][0] + j * ratios[1][0];
+            matr[1] = i * ratios[0][1] + j * ratios[1][1];
+            matr[2] = i * ratios[0][2] + j * ratios[1][2];
 
-            sums[0] = i * matr[0][0] + j * matr[1][0];
-            sums[1] = i * matr[0][1] + j * matr[1][1];
-            sums[2] = i * matr[0][2] + j * matr[1][2];
-
-            if (i + j > minm) {
+            if (i + j > sol) {
                 break;
             }
 
-            for (int k = 0; k < 100; k++)
+            for (k = 0; k < 100; k++)
             {
-                if (i + j + k >= minm) {
+                if (i + j + k >= sol)
+                {
                     break;
                 }
-                    
-                int t = (sums[0] + sums[1] + sums[2]) / rtot;
 
-                if (t != 0 && sums[0] == t * read[0] &&
-                    sums[1] == t * read[1] && sums[2] == t * read[2])
+                t = (matr[0] + matr[1] + matr[2]) / tot;
+                if (t != 0 && matr[0] == t * goal[0] && matr[1] == t * goal[1] && matr[2] == t * goal[2])
                 {
-                    minm = s;
+                    sol = s;
 
-                    mloc[0] = i;
-                    mloc[1] = j;
-                    mloc[2] = k;
-                    mloc[3] = t;
+                    solm[0] = i;
+                    solm[1] = j;
+                    solm[2] = k;
+                    solm[3] = t;
                 }
-                sums[0] += matr[2][0];
-                sums[1] += matr[2][1];
-                sums[2] += matr[2][2];
+
+                matr[0] += ratios[2][0];
+                matr[1] += ratios[2][1];
+                matr[2] += ratios[2][2];
             }
         }
     }
-        
-    if (minm == 301) {
+
+    if (sol == 301)
+    {
         fout << "NONE" << endl;
     }
-    else {
-        fout << mloc[0] << " " <<
-                mloc[1] << " " <<
-                mloc[2] << " " <<
-                mloc[3];
+    else
+    {
+        fout << solm[0] << " " << solm[1] << " " << solm[2] << " " << solm[3] << endl;
     }
 }
