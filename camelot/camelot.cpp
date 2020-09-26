086cb13604b0545dba52cf6fa09c942aad27921b
Steven He <therealonestig@gmail.com>
2020-09-25 21:56:44 -0700

    Camelot

--- camelot/camelot.cpp
+++ camelot/camelot.cpp
@@ -5,67 +5,87 @@ LANG: C++
 */
 
 #include <fstream>
-#include <string>
-#include <map>
-#include <vector>
+#include <iostream>
 #include <cstring>
-#include <algorithm>
+#include <cmath>
+#include <queue>
 
 using namespace std;
 
 ofstream fout("camelot.out");
 ifstream fin("camelot.in");
 
-struct Cam
-{
-    int r, c;
-} knights[1001], knight;
+using namespace std;
 
-int kn, R, C;
+struct Pos
+{
+    Pos(int a = 0, int b = 0) :
+        r(a),
+        c(b){};
+
+    int r, c;
+};
+
+Pos kni[1001];
+Pos king;
+int kn, R, C, num;
+int m[32][32][32][32];
+int kstep[32][32];
+int dr[9] = {-2, -1, 1, 2, 2, 1, -1, -2};
+int dc[9] = {1, 2, 2, 1, -1, -2, -2, -1};
+char cr;
 
 int main()
 {
-    fin >> R >> C;
-    char c;
-    int r;
-
-    fin >> c >> r;
-    knight.r = r;
-    knight.c = c - 'A' + 1;
-
-    while (fin >> c >> r)
-    {
-        knights[++kn].c = c - 'A' + 1;
-        knights[kn].r = r;
-    }
-
     memset(m, 1, sizeof(m));
+
+    fin >> R >> C >> cr >> num;
+    king.r = num;
+    king.c = (cr - 'A') + 1;
+
+    while (fin >> cr >> num)
+    {
+        kn++;
+        kni[kn].c = cr - 'A' + 1;
+        kni[kn].r = num;
+    }
 
     for (int i = 1; i <= R; i++)
     {
         for (int j = 1; j <= C; j++)
         {
+            queue<Pos> Q;
+            Q.push(Pos(i, j));
+            
             m[i][j][i][j] = 0;
+
             while (!Q.empty())
             {
-                for (int d = 0; d != 8; d++)
+                Pos select = Q.front();
+                Q.pop();
+
+                for (int d = 0; d < 8; d++)
                 {
-                    int tr = p.r + dr[d];
-                    int tc = p.c + dc[d];
+                    int tr = select.r + dr[d];
+                    int tc = select.c + dc[d];
+
                     if (tr > 0 && tc > 0 && tr <= R && tc <= C && m[i][j][tr][tc] == 0x01010101)
                     {
-                        m[i][j][tr][tc] = m[i][j][p.r][p.c] + 1;
-                        Q.push(P(tr, tc));
+                        m[i][j][tr][tc] = m[i][j][select.r][select.c] + 1;
+                        Q.push(Pos(tr, tc));
                     }
                 }
             }
         }
     }
 
-    int ker = knight.r + l > R ? R : knight.r + l;
-    int kec = knight.c + l > C ? C : knight.c + l;
-    int ksr = 0;
+    int l = 2;
+    int ksr = king.r - l < 1 ? 1 : king.r - l;
+    int ksc = king.c - l < 1 ? 1 : king.c - l;
+    int ker = king.r + l > R ? R : king.r + l;
+    int kec = king.c + l > C ? C : king.c + l;
     int minstep = 1 << 25;
+
     for (int i = 1; i <= R; i++)
     {
         for (int j = 1; j <= C; j++)
@@ -73,27 +93,26 @@ int main()
             int step = 0;
             for (int k = 1; k <= kn; k++)
             {
-                step += m[knights[k].r][knights[k].c][i][j];
+                step += m[kni[k].r][kni[k].c][i][j];
             }
 
-            int minn = max(abs(knight.r - i), abs(knight.c - j));
+            int minn = max(abs(king.r - i), abs(king.c - j));
             for (int pi = ksr; pi <= ker; pi++)
             {
                 for (int pj = ksc; pj <= kec; pj++)
                 {
                     for (int k = 1; k <= kn; k++)
                     {
-                        int tmp = max(abs(knight.r - pi), abs(knight.c - pj)) + m[knights[k].r][knights[k].c][pi][pj] + m[pi][pj][i][j] - m[knights[k].r][knights[k].c][i][j];
+                        int tmp = max(abs(king.r - pi), abs(king.c - pj)) + m[kni[k].r][kni[k].c][pi][pj] + m[pi][pj][i][j] - m[kni[k].r][kni[k].c][i][j];
                         if (tmp < minn)
+                        {
                             minn = tmp;
+                        }
                     }
                 }
             }
 
-            if (step + minn < minstep)
-            {
-                minstep = minn + step;
-            }
+            minstep = min(minstep, minn + step);
         }
     }
 
