e5018d3fabe27df1a797ee4d0d7a7676eff2e9c7
Steven He <therealonestig@gmail.com>
2020-10-03 22:02:25 -0700

    Rockers

--- rockers/rockers.cpp
+++ rockers/rockers.cpp
@@ -1,6 +1,6 @@
 /*
 ID: stevenh6
-TASK: taskname
+TASK: rockers
 LANG: C++
 */
 
@@ -10,30 +10,31 @@ using namespace std;
 
 using ll = long long;
 using ld = long double;
-using db = double; 
- 
-using pi = pair<int,int>;
-using pl = pair<ll,ll>;
-using pd = pair<db,db>;
- 
+using db = double;
+
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+
 using vi = vector<int>;
 using vb = vector<bool>;
 using vl = vector<ll>;
-using vd = vector<db>; 
+using vd = vector<db>;
 using vs = vector<string>;
 using vpi = vector<pi>;
-using vpl = vector<pl>; 
+using vpl = vector<pl>;
 using vpd = vector<pd>;
 
-#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
-#define FOR0(i,a) FOR(i,0,a)
-#define RFOR(i,a,b) for (int i = (b)-1; i >= (a); --i)
-#define RFOR0(i,a) ROF(i,0,a)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FOR0(i, a) FOR(i, 0, a)
+#define RFOR(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define RFOR0(i, a) ROF(i, 0, a)
 
 ofstream fout;
 ifstream fin;
 
-void setIO(string t) {
+void setIO(string t)
+{
     fout.open(t + ".out");
     fin.open(t + ".in");
 }
@@ -45,26 +46,36 @@ int le[21];
 
 int main()
 {
-    setIO("taskname");
+    setIO("rockers");
 
     int n, t, m;
     int sol = 0;
     fin >> n >> t >> m;
 
-    for (int i = 0; i < n; i++) {
+    FOR0(i, n)
+    {
         fin >> le[i];
     }
 
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < t + 1; j++) {
-            for (int k = 0; k < n + 1; k++) {
-                for (int l = 0; l < n + 1; l++) {
-                    if (dp[i][j][k] + 1 > dp[i][j + le[l]][l]) {
-                        dp[i][j + le[l]][l] = dp[i][j][k] + 1;
+    FOR0(i, m)
+    {
+        FOR0(j, t + 1)
+        {
+            FOR0(k, n + 1)
+            {
+                FOR(l, k + 1, n + 1)
+                {
+                    if (j + le[l] <= t)
+                    {
+                        dp[i][j + le[l]][l] = max(dp[i][j][k] + 1, dp[i][j + le[l]][l]);
+
+                        continue;
                     }
 
-                    sol = max(dp[i][j][k], sol);
+                    dp[i + 1][le[l]][l] = max(dp[i][j][k] + 1, dp[i + 1][le[l]][l]);
                 }
+                
+                sol = max(sol, dp[i][j][k]);
             }
         }
     }
